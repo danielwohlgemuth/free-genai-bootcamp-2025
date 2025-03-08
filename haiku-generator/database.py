0667d859552ea8ec90d861375826de5e7ca87eec
Daniel Wohlgemuth <daniel.wohlgemuth.epp@gmail.com>
2025-03-08 08:28:32 -0300

    Make database return row as dictionary

--- haiku-generator/database.py
+++ haiku-generator/database.py
@@ -10,6 +10,7 @@ DATABASE_URL = os.getenv("DATABASE_URL", "haiku_generator.db")
 
 def get_db_connection():
     conn = sqlite3.connect(DATABASE_URL)
+    conn.row_factory = sqlite3.Row
     return conn
 
 def create_tables():
